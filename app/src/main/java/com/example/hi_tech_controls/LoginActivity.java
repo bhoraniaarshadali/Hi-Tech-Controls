5dc1786e94ade3bc0b38ff9974f682c8722c5a64
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-08-15 17:31:53 +0530

    Dashboard cardView (.xml)

--- app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
@@ -1,15 +1,29 @@
 package com.example.hi_tech_controls;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginButton1 = findViewById(R.id.loginButton);
+
+        loginButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hi_tech_controls/MainActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.hi_tech_controls;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView backButton1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backButton1 = findViewById(R.id.backButton);
+
+        backButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
