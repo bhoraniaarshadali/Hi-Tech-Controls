f027cb383c50c6e1ba9bb192609c169f2ad4953e
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-09-09 16:10:18 +0530

    Fragment templateSet 1,2,3,4 | nextOn add textSwitcher @header

--- app/src/main/java/com/example/hi_tech_controls/AddDetailsActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/AddDetailsActivity.java
@@ -1,7 +1,6 @@
 package com.example.hi_tech_controls;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,30 +13,38 @@ public class AddDetailsActivity extends AppCompatActivity {
     ImageView addClientDtls_Back1;
     ImageView addClientDtls_Next1;
 
+    // Fragments
+    Fragment fillOneFragment;
+    Fragment fillTwoFragment;
+    Fragment fillThreeFragment;
+    Fragment fillFourFragment;
+
+    // Current fragment index
+    int currentFragmentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_details);
 
+        // Initialize Fragments
+        fillOneFragment = new com.example.hi_tech_controls.fragments.fill_one_fragment();
+        fillTwoFragment = new com.example.hi_tech_controls.fragments.fill_two_fragment();
+        fillThreeFragment = new com.example.hi_tech_controls.fragments.fill_three_fragment();
+        fillFourFragment = new com.example.hi_tech_controls.fragments.fill_four_fragment();
+
         // Load First Fragment (Fill_one)
-        loadFragment(new com.example.hi_tech_controls.fragments.fill_one_fragment());
+        loadFragment(fillOneFragment);
 
-        //find BACK
+        // Find BACK
         addClientDtls_Back1 = findViewById(R.id.addClientDtls_Back);
-        //implement
-        addClientDtls_Back1.setOnClickListener(v -> onBackPressed());
+        // Implement BACK button
+        addClientDtls_Back1.setOnClickListener(v -> goBack());
 
-        //find NEXT
+        // Find NEXT
         addClientDtls_Next1 = findViewById(R.id.addClientDtls_Next);
-        //implement
-        addClientDtls_Next1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Load Second Fragment (Fill_two)
-                //loadFragment(new com.example.hi_tech_controls.fragments.fill_two_fragment());
-            }
-        });
+        // Implement NEXT button
+        addClientDtls_Next1.setOnClickListener(v -> loadNextFragment());
     }
 
     // Fragment Method
@@ -49,5 +56,36 @@ public class AddDetailsActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
+    }
+
+    // Method to load the next fragment in sequence
+    private void loadNextFragment() {
+        currentFragmentIndex++;
+
+        if (currentFragmentIndex == 1) {
+            loadFragment(fillTwoFragment);
+        } else if (currentFragmentIndex == 2) {
+            loadFragment(fillThreeFragment);
+        } else if (currentFragmentIndex == 3) {
+            loadFragment(fillFourFragment);
+        }
+    }
+
+    // Method to navigate back to the previous fragment
+    private void goBack() {
+        if (currentFragmentIndex > 0) {
+            currentFragmentIndex--;
+
+            if (currentFragmentIndex == 0) {
+                loadFragment(fillOneFragment);
+            } else if (currentFragmentIndex == 1) {
+                loadFragment(fillTwoFragment);
+            } else if (currentFragmentIndex == 2) {
+                loadFragment(fillThreeFragment);
+            }
+        } else {
+            // If the current fragment index is 0, handle as needed (e.g., go back to the previous activity)
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/hi_tech_controls/fragments/fill_four_fragment.java
@@ -0,0 +1,23 @@
+package com.example.hi_tech_controls.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.hi_tech_controls.R;
+
+public class fill_four_fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fill_four, container, false);
+    }
+}
