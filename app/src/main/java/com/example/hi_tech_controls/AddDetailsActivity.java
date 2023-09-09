d62e90fd994eaf0bace82852a92f1ab17ee3a0cb
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-09-09 13:31:21 +0530

    Fill_One | != fragment | test |

--- app/src/main/java/com/example/hi_tech_controls/AddDetailsActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/AddDetailsActivity.java
@@ -1,13 +1,22 @@
 package com.example.hi_tech_controls;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddDetailsActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddDetailsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     ImageView addClientDtls_Back1;
+
+    // date picker
+    private EditText dateTextField1;
+    private DatePickerDialog datePickerDialog1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +24,27 @@ public class AddDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_details);
 
         addClientDtls_Back1 = findViewById(R.id.addClientDtls_Back);
-
         addClientDtls_Back1.setOnClickListener(v -> onBackPressed());
+
+        dateTextField1 = findViewById(R.id.dateTextField);
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog1 = new DatePickerDialog(this, (view, year1, month1, dayOfMonth) -> {
+            month1 += 1;
+            String date = dayOfMonth + "/" + month1 + "/" + year1;
+            dateTextField1.setText(date);
+        }, year, month, day);
+        datePickerDialog1.setCancelable(true);
+
+        dateTextField1.setOnClickListener(v -> datePickerDialog1.show());
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
     }
 }
