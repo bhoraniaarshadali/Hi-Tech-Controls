81d36c8eeec251439448e9e182890cb142f3de81
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-09-12 17:23:19 +0530

    fill_two_fragment- |inputSupply + outputSupply |

--- app/src/main/java/com/example/hi_tech_controls/fragments/fill_two_fragment.java
+++ app/src/main/java/com/example/hi_tech_controls/fragments/fill_two_fragment.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,16 +18,43 @@ import com.example.hi_tech_controls.R;
 
 public class fill_two_fragment extends Fragment {
 
+    private RadioGroup radioGroup;
+    private EditText localEditText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_fill_two, container, false);
 
+        // Initialize UI elements
+        radioGroup = rootView.findViewById(R.id.radioGroupCMD);
+        localEditText = rootView.findViewById(R.id.localEditText);
         Spinner spinner = rootView.findViewById(R.id.spinner);
+        RadioButton radioButtonLocal = rootView.findViewById(R.id.radioButtonLocal);
+        RadioButton radioButtonRemote = rootView.findViewById(R.id.radioButtonRemote);
+        RadioButton radioButtonComm = rootView.findViewById(R.id.radioButtonComm);
+
+
+        // Set up radio button listener
+        // Set up radio button listener
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Check which radio button is selected
+                if (checkedId == R.id.radioButtonLocal) {
+                    // If "Local" is selected, show the EditText field
+                    localEditText.setVisibility(View.VISIBLE);
+                } else {
+                    // If other options are selected, hide the EditText field
+                    localEditText.setVisibility(View.GONE);
+                }
+            }
+        });
+
 
         // Define an array of employee names
         String[] employees = {"Select employee name", "employee 1", "employee 2", "employee 3", "employee 4"};
 
-        // Create an ArrayAdapter to populate the Spinner with the employee names
+        // Create an ArrayAdapter to populate the Spinner with employee names
         ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, employees);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
