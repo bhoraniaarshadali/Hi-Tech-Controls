3b349dbf03df53704ec4614087d60773885b80df
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-09-15 13:07:57 +0530

    Logout Dialog-box

--- app/src/main/java/com/example/hi_tech_controls/MainActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.hi_tech_controls;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class
-MainActivity extends AppCompatActivity {
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class MainActivity extends AppCompatActivity {
 
     Button addClientBtn1;
     Button viewClientBtn1;
@@ -25,7 +27,6 @@ MainActivity extends AppCompatActivity {
 
         logout_btn_layout.setOnClickListener(v -> onBackPressed());
 
-
         addClientBtn1.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, AddDetailsActivity.class);
             startActivity(intent);
@@ -35,5 +36,45 @@ MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ViewDetailsActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        showExitConfirmationDialog();
+    }
+
+    private void showExitConfirmationDialog() {
+        SweetAlertDialog dialog = new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE);
+        dialog.setTitleText("Logout");
+        dialog.setContentText("Are you sure you want to logout the app?");
+
+        // Set custom text color for Confirm button
+        dialog.setConfirmButtonBackgroundColor(Color.parseColor("#E91E63"));
+        dialog.setConfirmText("Logout");
+
+        // Set custom text color for Cancel button
+        dialog.setCancelButtonBackgroundColor(Color.parseColor("#7C7C7C"));
+        dialog.setCancelText("Cancel");
+
+        dialog.showCancelButton(true);
+
+        dialog.setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sDialog) {
+                // Handle cancel button click (dismiss the dialog)
+                sDialog.dismissWithAnimation();
+            }
+        });
+
+        dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sDialog) {
+                // Handle confirm button click (exit the app)
+                sDialog.dismissWithAnimation();
+                finish(); // Close the app
+            }
+        });
+
+        dialog.show();
     }
 }
