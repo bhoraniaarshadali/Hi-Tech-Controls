87b42f9e6bd10efdea3bfcdf3959b2253de3a475
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-09-15 10:13:48 +0530

    fill_three_fragment- layout Responsiveness

--- app/src/main/java/com/example/hi_tech_controls/fragments/fill_four_fragment.java
+++ app/src/main/java/com/example/hi_tech_controls/fragments/fill_four_fragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -18,6 +22,38 @@ public class fill_four_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fill_four, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fill_four, container, false);
+        setUpSpinner(rootView);
+        return rootView;
+    }
+
+    private void setUpSpinner(View rootView) {
+        Spinner spinner = rootView.findViewById(R.id.spinner);
+        String[] employees = {"Select employee name", "employee 1", "employee 2", "employee 3", "employee 4"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, employees);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                handleSpinnerItemSelected(selectedItemView, position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing
+            }
+        });
+    }
+
+    private void handleSpinnerItemSelected(View selectedItemView, int position) {
+        if (selectedItemView instanceof TextView) {
+            if (position == 0) {
+                ((TextView) selectedItemView).setTextColor(getResources().getColor(R.color.grey));
+            } else {
+                ((TextView) selectedItemView).setTextColor(getResources().getColor(R.color.blue));
+            }
+        }
     }
 }
