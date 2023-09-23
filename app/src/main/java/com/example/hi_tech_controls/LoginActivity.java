65f4bb6c1caf3d1d224f572d18b79eb426a2777e
Dhawalika verma <dhawalikaverma102@gmail.com>
2023-09-23 22:28:22 +0530

    added the login credentials

--- app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
@@ -1,28 +1,55 @@
 package com.example.hi_tech_controls;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button loginButton1;
+    EditText emailEditText, passwordEditText;
+    Button loginButton;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginButton1 = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
 
-        loginButton1.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                username = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+
+                if ("Admin".equals(username) && "Admin".equals(password)) {
+
+                    Toast.makeText(LoginActivity.this, "Successfully Login", Toast.LENGTH_LONG).show();
+
+
+                    SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("isLoggedIn", true);
+                    editor.apply();
+
+                    Log.d("LoginActivity", "Login status saved: " + true);
+
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
