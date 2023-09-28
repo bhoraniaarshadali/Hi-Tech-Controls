f76b3eeabb433b3a004afc7b1128d04bf694d1e5
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-09-29 04:12:58 +0530

    LoginActivity | Add-Animation | Add SnakeBar

--- app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
     EditText emailEditText, passwordEditText;
@@ -26,28 +31,43 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         loginButton = findViewById(R.id.loginButton);
 
-        loginButton.setOnClickListener(v -> {
-//            LoginCred();
 
-            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        loginButton.setOnClickListener(v -> {
+            //LoginCred();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         });
 
+        anim();
+
     }
 
-    public void LoginCred(){
+    public void LoginCred() {
         username = emailEditText.getText().toString();
         password = passwordEditText.getText().toString();
 
         // Check if username or password is empty
-        if (username.isEmpty() || password.isEmpty()) {
-            Toast.makeText(LoginActivity.this, "Please enter both Username and Password", Toast.LENGTH_LONG).show();
+        if (username.isEmpty()) {
+            //Toast.makeText(LoginActivity.this, "Please enter username!", Toast.LENGTH_LONG).show();
+            Snackbar.make(emailEditText, "Please enter username!", Snackbar.LENGTH_LONG).show();
+            YoYo.with(Techniques.Shake)
+                    .duration(200)
+                    .repeat(2)
+                    .playOn(findViewById(R.id.emailEditText));
+        } else if (password.isEmpty()) {
+            //Toast.makeText(LoginActivity.this, "Please enter password!", Toast.LENGTH_LONG).show();
+            Snackbar.make(emailEditText, "Please enter password!", Snackbar.LENGTH_LONG).show();
+            YoYo.with(Techniques.Shake)
+                    .duration(200)
+                    .repeat(2)
+                    .playOn(findViewById(R.id.passwordEditText));
         } else if ("Admin".equals(username) && "Admin".equals(password)) {
             Toast.makeText(LoginActivity.this, "Successfully Login", Toast.LENGTH_LONG).show();
 
-            SharedPreferences preferences = getSharedPreferences("Login",MODE_PRIVATE);
+            SharedPreferences preferences = getSharedPreferences("Login", MODE_PRIVATE);
             SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("flag",true);
+            editor.putBoolean("flag", true);
             editor.apply();
             Intent Home = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(Home);
@@ -58,7 +78,82 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             // Display a message for incorrect login credentials
-            Toast.makeText(LoginActivity.this, "Wrong login credentials, please try again", Toast.LENGTH_LONG).show();
+            //Toast.makeText(LoginActivity.this, "Wrong login credentials, please try again", Toast.LENGTH_LONG).show();
+            Snackbar.make(emailEditText, "Wrong login credentials, please try again", Snackbar.LENGTH_LONG).show();
+            emailEditText.setText("");
+            passwordEditText.setText("");
+            YoYo.with(Techniques.Shake)
+                    .duration(200)
+                    .repeat(2)
+                    .playOn(findViewById(R.id.emailEditText));
+            YoYo.with(Techniques.Shake)
+                    .duration(200)
+                    .repeat(2)
+                    .playOn(findViewById(R.id.passwordEditText));
         }
+    }
+
+    public void anim() {
+        TextView welcomeTextView1 = findViewById(R.id.welcomeTextView);
+        TextView subHeadingTextView1 = findViewById(R.id.subHeadingTextView);
+        ImageView logoImageView1 = findViewById(R.id.logoImageView);
+        TextView usernameTitleTextView1 = findViewById(R.id.usernameTitleTextView);
+        EditText emailEditText1 = findViewById(R.id.emailEditText);
+        TextView passwordTitleTextView1 = findViewById(R.id.passwordTitleTextView);
+        EditText passwordEditText1 = findViewById(R.id.passwordEditText);
+        Button loginButton1 = findViewById(R.id.loginButton);
+        FrameLayout bottomShape1 = findViewById(R.id.bottomShape);
+        TextView versionTextView1 = findViewById(R.id.versionTextView);
+
+        //logo ImageView
+        logoImageView1.setAlpha(0f);
+        logoImageView1.setTranslationY(50);
+        logoImageView1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //welcome Text
+        welcomeTextView1.setAlpha(0f);
+        welcomeTextView1.setTranslationY(50);
+        welcomeTextView1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //subHeading Text
+        subHeadingTextView1.setAlpha(0f);
+        subHeadingTextView1.setTranslationY(50);
+        subHeadingTextView1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //usernameTitleTextView1
+        usernameTitleTextView1.setAlpha(0f);
+        usernameTitleTextView1.setTranslationY(50);
+        usernameTitleTextView1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //username EditText
+        emailEditText1.setAlpha(0f);
+        emailEditText1.setTranslationY(50);
+        emailEditText1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //passwordTitleTextView1
+        passwordTitleTextView1.setAlpha(0f);
+        passwordTitleTextView1.setTranslationY(50);
+        passwordTitleTextView1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //password EditText
+        passwordEditText1.setAlpha(0f);
+        passwordEditText1.setTranslationY(50);
+        passwordEditText1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //login Button
+        loginButton1.setAlpha(0f);
+        loginButton1.setTranslationY(50);
+        loginButton1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //bottom Shape
+        bottomShape1.setAlpha(0f);
+        bottomShape1.setTranslationY(50);
+        bottomShape1.animate().alpha(1f).translationYBy(-50).setDuration(1000);
+
+        //version TextView
+        versionTextView1.setAlpha(0f);
+        versionTextView1.setTranslationY(70);
+        versionTextView1.animate().alpha(1f).translationYBy(-50).setDuration(2000);
+
     }
 }
--- app/src/main/java/com/example/hi_tech_controls/ViewDetailsActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/ViewDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.hi_tech_controls;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,16 @@ public class ViewDetailsActivity extends AppCompatActivity {
 
     ImageView viewClientDtls_Back1;
 
+    Button searchButton1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_details);
 
         viewClientDtls_Back1 = findViewById(R.id.viewClientDtls_Back);
+        searchButton1 = findViewById(R.id.searchButton);
+
 
         viewClientDtls_Back1.setOnClickListener(v -> onBackPressed());
     }
