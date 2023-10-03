50d8e45867e9521c6f52a3f9f34083b4e2fc58bb
Arshadali <bhoraniyaarshadali431@gmail.com>
2023-10-04 03:18:19 +0530

    Cleanup

--- app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/LoginActivity.java
@@ -3,9 +3,7 @@ package com.example.hi_tech_controls;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -15,6 +13,7 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.material.snackbar.Snackbar;
@@ -45,6 +44,9 @@ public class LoginActivity extends AppCompatActivity {
 
         loginButton.setOnClickListener(v -> {
             LoginCred();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
         });
 
         anim();
