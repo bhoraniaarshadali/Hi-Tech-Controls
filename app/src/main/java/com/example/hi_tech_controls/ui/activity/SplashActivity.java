e9eba30b5b1cd173053528f5738203cd4b88fe46
Arshad ali bhorania <bhoraniyaarshadali431@gmail.com>
2026-05-06 14:44:01 +0530

    feat: show app version on splash screen and use PackageManager for robustness

--- app/src/main/java/com/example/hi_tech_controls/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/hi_tech_controls/ui/activity/SplashActivity.java
@@ -2,8 +2,12 @@ package com.example.hi_tech_controls.ui.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,12 +15,22 @@ import com.airbnb.lottie.LottieAnimationView;
 import com.example.hi_tech_controls.R;
 
 public class SplashActivity extends AppCompatActivity {
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         LottieAnimationView anim = findViewById(R.id.splashAnim);
+        TextView versionText = findViewById(R.id.versionText);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            versionText.setText("Version " + version);
+        } catch (PackageManager.NameNotFoundException e) {
+            versionText.setText("Version 1.0");
+        }
 
         anim.setImageAssetsFolder("splash/images");
 
